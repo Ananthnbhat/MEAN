3896312e0901372a4deb63de3df0bdd2cc7bafc4
Ananth Bhat <anant@Vivobook.S15>
2018-04-15 23:23:36 +0530

    n

--- Angluar tut/src/app/heroes/heroes.component.ts
+++ Angluar tut/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from "../mock-heroes";
 
 @Component({
   
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero :Hero = {
+    id:1,
+    name:'Windstorm'
+  };
+  heroes=HEROES;
   constructor() { }
 
   ngOnInit() {
