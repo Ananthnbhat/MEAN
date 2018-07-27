b35faca122e6a529550d72803b2c9495228316ff
Ananth Bhat <anant@Vivobook.S15>
2018-07-27 21:33:51 +0530

    revised heroService

---
+++ angular/tap-room/src/app/hero.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+import { Keg } from "./models/keg.model";
+import { KEGS } from "./models/kegs";
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+// import { MessageService } from "./message.service";
---
+++ angular/tap-room/src/app/models/kegs.ts
@@ -0,0 +1,8 @@
+import { Keg } from "./keg.model";
+
+export const KEGS: Keg[] = [
+    { name: 'Bud Light Platinum',brand:"Anheuser-Busch" ,price:8 ,alcoholContent:"High" ,pints:124 },
+    { name:"Budweiser" ,brand:"Anheuser-Busch" ,price:4 ,alcoholContent:"Low" ,pints:8 },
+    { name:"Asahi" ,brand:"Asahi Breweries, Ltd" ,price:5 ,alcoholContent:"Medium" ,pints:124 },
+    { name:"Asahi" ,brand:"Asahi Breweries, Ltd" ,price:5 ,alcoholContent:"Medium" ,pints:124 }
+  ];
