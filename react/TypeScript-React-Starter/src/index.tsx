6da90daf64b6983b1979d3132403473ea05e1c6e
Ananthnbhat <ananthnbhat2@gmail.com>
2019-01-15 18:37:38 +0530

    add stateless and stateful Hello

--- react/TypeScript-React-Starter/src/index.tsx
+++ react/TypeScript-React-Starter/src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import Hello from './components/Hello';
 
 ReactDOM.render(
-  <App />,
+  // <App />,
+  // document.getElementById('root') as HTMLElement
+  <Hello name="Typescript" enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
